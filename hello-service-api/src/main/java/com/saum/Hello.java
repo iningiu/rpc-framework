b49657ed095ccc44600375dad3843668236fd5c2
iningiu <inmingliu@gmail.com>
2021-07-17 14:08:41 +0800

    添加测试服务接口

---
+++ hello-service-api/src/main/java/com/saum/Hello.java
@@ -0,0 +1,13 @@
+package com.saum;
+
+import java.io.Serializable;
+
+/**
+ * @Author saum
+ * @Date 2021/7/17
+ * @Description:
+ */
+public class Hello implements Serializable {
+    private String message;
+    private String description;
+}
---
+++ hello-service-api/src/main/java/com/saum/HelloService.java
@@ -0,0 +1,9 @@
+package com.saum;
+
+/**
+ * @Author saum
+ * @Date 2021/7/17
+ */
+public interface HelloService {
+    String hello(Hello hello);
+}
