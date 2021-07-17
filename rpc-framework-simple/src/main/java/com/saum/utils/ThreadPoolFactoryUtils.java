c20f28ef99ce4d428a92294ea3012cca0310bfd6
iningiu <inmingliu@gmail.com>
2021-07-17 17:19:26 +0800

    socket通信改进之线程池

---
+++ rpc-framework-simple/src/main/java/com/saum/utils/ThreadPoolFactoryUtils.java
@@ -0,0 +1,81 @@
+package com.saum.utils;
+
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Map;
+import java.util.concurrent.*;
+
+/**
+ * @Author saum
+ * @Date 2021/7/17
+ * @Description: 创建线程池的工具类
+ */
+@Slf4j
+public class ThreadPoolFactoryUtils {
+
+    /**
+     * 通过 threadNamePrefix 来区分不同线程池（我们可以把相同 threadNamePrefix 的线程池看作是为同一业务场景服务）。
+     * key: threadNamePrefix
+     * value: threadPool
+     */
+    private static final Map<String, ExecutorService> THREAD_POOLS = new ConcurrentHashMap<>();
+
+    private ThreadPoolFactoryUtils(){}
+
+    public static ExecutorService createCustomThreadPoolIfAbsent(String threadNamePrefix){
+        CustomThreadPoolConfig customThreadPoolConfig = new CustomThreadPoolConfig();
+        return createCustomThreadPoolIfAbsent(customThreadPoolConfig, threadNamePrefix, false);
+    }
+
+    public static ExecutorService createCustomThreadPoolIfAbsent(CustomThreadPoolConfig customThreadPoolConfig, String threadNamePrefix, Boolean daemon){
+        ExecutorService threadPool = THREAD_POOLS.computeIfAbsent(threadNamePrefix, k -> createThreadPool(customThreadPoolConfig, threadNamePrefix, daemon));
+        // 如果 threadPool 被 shutdown 的话就重新创建一个
+        if(threadPool.isShutdown() || threadPool.isTerminated()){
+            THREAD_POOLS.remove(threadNamePrefix);
+            threadPool = createThreadPool(customThreadPoolConfig, threadNamePrefix, daemon);
+            THREAD_POOLS.put(threadNamePrefix, threadPool);
+        }
+        return threadPool;
+    }
+
+    public static ExecutorService createThreadPool(CustomThreadPoolConfig customThreadPoolConfig, String threadNamePrefix, Boolean daemon){
+        ThreadFactory threadFactory = createThreadFactory(threadNamePrefix, daemon);
+        return new ThreadPoolExecutor(customThreadPoolConfig.getCorePoolSize(), customThreadPoolConfig.getMaximumPoolSize(),
+                customThreadPoolConfig.getKeepAliveTime(), customThreadPoolConfig.getUnit(), customThreadPoolConfig.getWorkQueue(),
+                threadFactory);
+    }
+
+    /**
+     * 创建 ThreadFactory 。如果threadNamePrefix不为空则使用自建ThreadFactory，否则使用defaultThreadFactory
+     *
+     * @param threadNamePrefix 作为创建的线程名字的前缀
+     * @param daemon           指定是否为 Daemon Thread(守护线程)
+     * @return ThreadFactory
+     */
+    public static ThreadFactory createThreadFactory(String threadNamePrefix, Boolean daemon){
+        if(threadNamePrefix != null && !"".equals(threadNamePrefix)){
+            if(daemon != null){
+                return new ThreadFactoryBuilder().setNameFormat(threadNamePrefix + "-%d").setDaemon(daemon).build();
+            }else{
+                return new ThreadFactoryBuilder().setNameFormat(threadNamePrefix + "-%d").build();
+            }
+        }
+        return Executors.defaultThreadFactory();
+    }
+
+    public static void shutDownAllThreadPool(){
+        log.info("call shutDownAllThreadPool method");
+        THREAD_POOLS.entrySet().parallelStream().forEach(entry -> {
+            ExecutorService executorService = entry.getValue();
+            executorService.shutdown();
+            log.info("shut down thread pool [{}] [{}]", entry.getKey(), executorService.isTerminated());
+            try {
+                executorService.awaitTermination(10, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                log.error("Thread pool never terminated");
+                executorService.shutdownNow();
+            }
+        });
+    }
+}
--- rpc-framework-simple/src/test/java/com/saum/socket/HelloServer.java
+++ rpc-framework-simple/src/test/java/com/saum/socket/HelloServer.java
@@ -1,5 +1,6 @@
 package com.saum.socket;
 
+import com.saum.utils.ThreadPoolFactoryUtils;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
@@ -7,6 +8,7 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
 
 /**
  * @Author saum
@@ -16,26 +18,23 @@ import java.net.Socket;
 @Slf4j
 public class HelloServer {
 
+    private final ExecutorService threadPool;
+
+    public HelloServer() {
+        threadPool = ThreadPoolFactoryUtils.createCustomThreadPoolIfAbsent("");
+    }
+
     public void start(int port){
         // 1.创建ServerSocket对象并绑定一个端口
         try(ServerSocket server = new ServerSocket(port)){
             Socket socket;
             // 2.通过accpet()方法监听客户端请求，accept会阻塞，直到收到客户端的连接请求
             while((socket = server.accept()) != null){
-                log.info("建立连接成功！");
-                try(ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
-                    ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream())){
-                    // 3.通过输入流读取客户端发送的请求信息
-                    Message message = (Message) objectInputStream.readObject();
-                    log.info("服务器收到消息：" + message.getContent());
-                    message.setContent("hi, I'm server!");
-                    // 4.通过输出流向客户端发送响应消息
-                    objectOutputStream.writeObject(message);
-                    objectOutputStream.flush();
-                }
+                log.info("建立连接成功[{}]", socket.getInetAddress());
+                threadPool.execute(new SocketHandlerRunnalbe(socket));
             }
-
-        }catch (IOException | ClassNotFoundException e){
+            threadPool.shutdownNow();
+        }catch (IOException e){
             log.error("IO异常:", e);
         }
     }
@@ -45,3 +44,30 @@ public class HelloServer {
         helloServer.start(6666);
     }
 }
+
+@Slf4j
+class SocketHandlerRunnalbe implements Runnable{
+    private final Socket socket;
+
+    SocketHandlerRunnalbe(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        try(ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream())){
+            // 3.通过输入流读取客户端发送的请求信息
+            Message message = (Message) objectInputStream.readObject();
+            log.info("服务器收到消息：" + message.getContent());
+            message.setContent("hi, I'm server!");
+            // 4.通过输出流向客户端发送响应消息
+            objectOutputStream.writeObject(message);
+            objectOutputStream.flush();
+        }catch (IOException | ClassNotFoundException e){
+            log.error("发生异常:", e);
+        }
+    }
+}
+
+
